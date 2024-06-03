54e8a314e78ce9b4815dcbce4c47d935eb354bb5
yun <array_xiangxiang@163.com>
2024-06-03 18:03:56 +0800

    VueX

---
+++ 09_vuecli_vuex/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Count/>
+  </div>
+</template>
+
+<script>
+import Count from '@/components/Count'
+
+export default {
+  name: 'App',
+  components:{
+    Count
+  }
+}
+</script>
+
+<style>
+</style>
