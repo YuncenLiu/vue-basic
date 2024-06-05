e9d751451ea5f82525cd485ea332ac04465c2b37
yun <array_xiangxiang@163.com>
2024-06-05 13:24:15 +0800

    Vue Router

---
+++ 10_vueCli_router/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue Router Demo</h2></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <a class="list-group-item" href="./about.html">About</a>
+          <a class="list-group-item active" href="./home.html">About</a>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <h2>我是 Home 的内容</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+</style>
